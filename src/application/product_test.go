c6f4e59749b15caba3278987b2cad4c70466d6a0
jellyBott <gus.fbarroso@gmail.com>
2023-10-21 23:09:40 -0300

    feat: add test for IsValid in product

--- src/application/product_test.go
+++ src/application/product_test.go
@@ -1,23 +1,26 @@
-package application
+package application_test
 
 import (
 	"testing"
+
+	"github.com/gustavo-f-b/Arquitetura-hexagonal/src/application"
+	uuid "github.com/satori/go.uuid"
 )
 
 func TestProduct_Enable(t *testing.T) {
 	tests := []struct {
 		name    string
-		product *Product
+		product *application.Product
 		wantErr bool
 	}{
 		{
 			name:    "Valid Product",
-			product: &Product{Name: "Widget", Price: 10.0},
+			product: &application.Product{Name: "Widget", Price: 10.0},
 			wantErr: false,
 		},
 		{
 			name:    "Invalid Product",
-			product: &Product{Name: "Widget", Price: -5.0},
+			product: &application.Product{Name: "Widget", Price: -5.0},
 			wantErr: true,
 		},
 	}
@@ -33,22 +36,22 @@ func TestProduct_Enable(t *testing.T) {
 func TestProduct_Disable(t *testing.T) {
 	tests := []struct {
 		name    string
-		product *Product
+		product *application.Product
 		wantErr bool
 	}{
 		{
 			name:    "Invalid Product",
-			product: &Product{Name: "Widget", Price: 10.0},
+			product: &application.Product{Name: "Widget", Price: 10.0},
 			wantErr: true,
 		},
 		{
 			name:    "Invalid Product",
-			product: &Product{Name: "Widget", Price: -5.0},
+			product: &application.Product{Name: "Widget", Price: -5.0},
 			wantErr: true,
 		},
 		{
 			name:    "Valid Product",
-			product: &Product{Name: "Widget", Price: 0},
+			product: &application.Product{Name: "Widget", Price: 0},
 			wantErr: false,
 		},
 	}
@@ -56,6 +59,35 @@ func TestProduct_Disable(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if err := tt.product.Disable(); (err != nil) != tt.wantErr {
 				t.Errorf("Product.Disable() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestProduct_IsValid(t *testing.T) {
+	tests := []struct {
+		name    string
+		product *application.Product
+		want    bool
+		wantErr bool
+	}{
+		{
+			name:    "Invalid Product",
+			product: &application.Product{Id: uuid.NewV4().String(), Name: "Widget", Status: application.DISABLED, Price: -10.0},
+			want:    false,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := tt.product.IsValid()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Product.IsValid() error = %v, wantErr %v", err, tt.wantErr)
+				return
+
+			}
+			if got != tt.want {
+				t.Errorf("Product.IsValid() = %v, want %v", got, tt.want)
 			}
 		})
 	}
